9b38b82c84c42b418ed3f3bdf197283fcf1eed6c
Minh Bang <contact@minhbang.com>
2016-02-27 08:18:19 +0700

    add en language

---
+++ lang/en/common.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'content'                    => 'Content',
+    'manage'                     => 'Content list <small>order by updated time</small>',
+    'main_info'                  => 'Main information <small>required</small>',
+    'additional_info'            => 'Additional information',
+    'title'                      => 'Title',
+    'slug'                       => 'Slug',
+    'body'                       => 'Body',
+    'user'                       => 'Author',
+    'metadata'                   => 'Published: :datetime — :hit view',
+    'not_found'                  => 'Content not found',
+    'unable_delete_guarded_item' => 'Unable delete guarded Content',
+];
